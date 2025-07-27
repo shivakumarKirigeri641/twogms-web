7482b7a4ca1dab3a0693e3a94611cfff01c1db33
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-07-27 11:19:11 +0530

    task:tailwind included.

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "flowbite";
+import ReactDOM from "react-dom/client";
+const AppLayout = () => {
+  return (
+    <div className="bg-sky-300">
+      <p>test</p>
+    </div>
+  );
+};
+ReactDOM.createRoot(document.getElementById("root")).render(<AppLayout />);
