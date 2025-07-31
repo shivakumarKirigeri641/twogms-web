c9f07658adf1ba1e5ee2c21e4f94241d2e5dbea4
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-07-31 22:50:16 +0530

    task:if refreshed, if no login, navigate to home

--- src/components/garage/GarageDashboard.js
+++ src/components/garage/GarageDashboard.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 
 const GarageDashboard = () => {
+  const navigate = useNavigate();
+  const garagecredentials = useSelector(
+    (store) => store.garageCredentialDetails
+  );
+  useEffect(() => {
+    if (!garagecredentials) {
+      navigate("/");
+    }
+  }, []);
   return (
     <div>
       <p>dashboard</p>
