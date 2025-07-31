09368241343ba5c00fce6d1a945406295611d20d
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-07-31 12:18:35 +0530

    task:removed flowbite concept

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
 import Footer from "./components/general/Footer";
-import "flowbite";
 import Home from "./components/general/Home";
-import Statistics from "./components/garagespecific/Statistics";
-import GarageDashboard from "./components/garagespecific/GarageDashboard";
 import Error from "./components/Error";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import appStore from "./store/appStore";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
-import CustomerLoginOrRegister from "./components/general/CustomerLoginOrRegister";
-import GarageLoginOrRegister from "./components/general/GarageLoginOrRegister";
 const AppLayout = () => {
   return (
     <Provider store={appStore}>
@@ -28,22 +23,6 @@ const appRouter = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
-      },
-      {
-        path: "/twogms/dashboard",
-        element: <GarageDashboard />,
-      },
-      {
-        path: "/twogms/statistics",
-        element: <Statistics />,
-      },
-      {
-        path: "/twogms/garage/loginorregister",
-        element: <GarageLoginOrRegister />,
-      },
-      {
-        path: "/twogms/customer/loginorregister",
-        element: <CustomerLoginOrRegister />,
       },
     ],
     errorElement: <Error />,
--- src/components/general/Home.js
+++ src/components/general/Home.js
@@ -1,23 +1,9 @@
-import React, { useEffect, useState } from "react";
-import DropDown from "../customcomponents/DropDown";
-import DataTable from "../customcomponents/DataTable";
-import getGaragePoints from "../../utils/getGaragePoints";
-import getCustomerPoints from "../../utils/getCustomerPoints";
-import whyChooseTwoGms from "../../utils/whyChooseTwoGms";
-import LoginOrRegister from "./GarageLoginOrRegister";
-import Jumbotron from "./homecontents/Jumbotron";
 import { useNavigate } from "react-router";
-import Navbar from "./homecontents/Navbar";
-import Jumbotron_garage from "./homecontents/Jumbotron_garage";
-import Jumbotron_customer from "./homecontents/Jumbotron_customer";
 const Home = () => {
   const navigate = useNavigate();
   return (
     <div>
-      <Navbar />
-      <Jumbotron />
-      <Jumbotron_garage />
-      <Jumbotron_customer />
+      <p>test</p>
     </div>
   );
 };
