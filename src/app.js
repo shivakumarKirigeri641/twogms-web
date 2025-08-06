c26b3fbfe993ffc3197ad84c326f2bf622ce3980
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-08-06 14:52:50 +0530

    task:tailwind installed.

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+const AppLayout = () => {
+  return (
+    <div>
+      <p>test</p>
+    </div>
+  );
+};
+ReactDOM.createRoot(document.getElementById("root")).render(<AppLayout />);
