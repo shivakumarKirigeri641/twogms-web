6b37433864a9a0216b3e24dfc61dafd0b72980c2
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-08-06 16:59:56 +0530

    task:tailwind installed.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,42 @@
 import ReactDOM from "react-dom/client";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import About from "./components/About";
+import Dashboard from "./components/Dashboard";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
+import { useEffect } from "react";
 const AppLayout = () => {
   return (
     <div>
-      <p>test</p>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
   );
 };
-ReactDOM.createRoot(document.getElementById("root")).render(<AppLayout />);
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "/twogms/login",
+        element: <Login />,
+      },
+      {
+        path: "/twogms/about",
+        element: <About />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={appRouter} />
+);
---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import { Button, Flex } from "antd";
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <p>test</p>
+      <button className="bg-pink-300">hello</button>
+    </div>
+  );
+};
+
+export default Login;
