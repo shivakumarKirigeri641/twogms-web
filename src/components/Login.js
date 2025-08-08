3513399a98879b3be530ccce3f3c26dec5d7aa57
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-08-08 16:09:18 +0530

    task:login sample done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,78 +1,93 @@
+import axios from "axios";
 import { useState } from "react";
 
 const Login = () => {
   const [showOtp, setshowOtp] = useState(false);
   const [showInvalidOtpError, setshowInvalidOtpError] = useState(false);
   const [showNotRegisteredError, setshowNotRegisteredError] = useState(false);
+  const handleLogin = async () => {
+    try {
+    } catch (err) {
+      setshowInvalidOtpError(err.message);
+    }
+  };
   return (
-    <div className="relative h-screen">
+    <div className="relative h-screen lg:w-[40%] lg:mx-auto bg-gradient-to-b from-[#245F7F] to-[#78CEB4]">
       <img
-        className="fixed w-full h-full inset-0 -z-10"
+        className="hidden lg:block fixed w-full h-full inset-0 -z-10"
         src={require("../images/login_background.jpg")}
       ></img>
-      <div className="w-full lg:w-[30%] flex justify-center items-center  mx-auto">
-        <div className="z-10 w-full">
-          <div className="flex justify-center items-center my-3">
-            <img
-              className="object-cover w-35 rounded-full"
-              src={require("../images/logo.png")}
-            ></img>
-          </div>
-          <div class="card m-3 mb-5">
-            <div class="card-body text-center">
-              <h5 class="card-title mb-2.5 text-center">Welcome to TWOGMS</h5>
-              <p>
-                All your two-wheeler service needs—managed online, anytime,
-                anywhere.
-              </p>
-              <p className="my-5 text-left underline underline-offset-2 font-bold text-blue-800 cursor-pointer italic">
-                why TWOGMMS?
-              </p>
-            </div>
-          </div>
-          <div class="input-floating w-full px-3">
+      <div className="pt-10">
+        <img
+          className="rounded-full w-25 mx-auto"
+          src={require("../images/logo.png")}
+        ></img>
+        <div className="text-white">
+          <p className="text-2xl font-bold text-center mt-8">
+            Welcome to TWOGMS
+          </p>
+          <p className="text-center italic"> - You service, we record</p>
+        </div>
+        {/**login */}
+        <div className="text-yellow-100">
+          <p className="text-2xl font-bold text-center mt-8">Garage Sign In</p>
+        </div>
+        <div className="text-yellow-100">
+          {/**sign in field */}
+          <div class="input-floating w-full px-2 mt-5">
             <input
               type="text"
-              placeholder="Enter your mobile number"
-              className="input"
+              placeholder="Enter 10-digit mobile number"
+              required
+              class="input"
               id="floatingInput"
             />
-            <label
-              className="input-floating-label p-2 bg-transparent"
-              for="floatingInput"
-            >
+            <label class="input-floating-label" for="floatingInput">
               Mobile number
             </label>
-            <button class="btn btn-primary w-full my-2">Get OTP</button>
-            {showNotRegisteredError && (
-              <div class="alert alert-error" role="alert">
-                Oops! It seems you have entered an invalid mobile number.
-              </div>
-            )}
           </div>
-          {/**confirm otp */}
-          {showOtp && (
-            <div class="input-floating w-full px-2">
+          {/**sign in field */}
+          {/**get otp */}
+          <div className="px-2">
+            <button class="btn btn-accent w-full my-2">Get OTP</button>
+          </div>
+          {/**get otp */}
+          <div>
+            {/**enter otp */}
+            <div class="input-floating w-full px-2 mt-5">
               <input
                 type="text"
-                placeholder="Enter your mobile number"
-                className="input"
+                placeholder="Enter 6-digit otp recieved"
+                required
+                class="input"
                 id="floatingInput"
               />
-              <label className="input-floating-label px-3" for="floatingInput">
+              <label class="input-floating-label" for="floatingInput">
                 OTP
               </label>
-              <button class="btn btn-accent w-full my-2">Submit</button>
-              {showInvalidOtpError && (
-                <div class="alert alert-error" role="alert">
-                  Invalid OTP provided!
-                </div>
-              )}
             </div>
-          )}
-          {/**confirm otp */}
+            {/**enter otp */}
+            {/**login */}
+            <div className="px-2">
+              <button class="btn btn-accent w-full my-2">Sign In</button>
+            </div>
+          </div>
+          {/**login */}
+        </div>
+        {/**login */}
+      </div>
+      {/**bottom contatct for login */}
+      <div className="fixed bottom-0 m-2 text-xs text-center">
+        <div className="flex justify-center items-center text-red-600 font-bold">
+          <p>Please call or </p>
+          <img
+            className="w-5 mx-2"
+            src={require("../images/icons/whatsapp.png")}
+          ></img>
+          <p>+919886122415 for Garage Sign In.</p>
         </div>
       </div>
+      {/**bottom contatct for login */}
     </div>
   );
 };
