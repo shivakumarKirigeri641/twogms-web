21c553384ff0636f6de8016a7f01fa316487b16c
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-08-24 08:20:45 +0530

    task:app test done with react

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+const AppLayout = () => {
+  return (
+    <div>
+      <p>app</p>
+    </div>
+  );
+};
+ReactDOM.createRoot(document.getElementById("root")).render(<AppLayout />);
